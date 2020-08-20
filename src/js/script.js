d718737ed14e0136e6e3dca687605d3345e41d2d
Vlada <vlada_chvanova@mail.ru>
2020-08-20 17:45:59 +0300

    сделала бургер

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,22 @@ let swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination',
     },
 });
+
+
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.header__item'),
+        menuItem = document.querySelectorAll('.header__item li'),
+        humburger = document.querySelector('.burger');
+
+    humburger.addEventListener('click', () => {
+        humburger.classList.toggle('burger_active');
+        menu.classList.toggle('header__item_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            humburger.classList.toggle('burger_active');
+            menu.classList.toggle('header__item_active');
+        });
+    });
+});
