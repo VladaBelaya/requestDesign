1cfc66e407b292bbf086b08c98700ecaba195a5f
Vlada <vlada_chvanova@mail.ru>
2020-08-24 19:22:00 +0300

    закончила работу

--- src/js/script.js
+++ src/js/script.js
@@ -24,16 +24,25 @@ window.addEventListener('DOMContentLoaded', () => {
         copyBtn = document.querySelector('.copy__btn'),
         mobile = document.querySelector('.header__mobile'),
         modal = document.querySelector('.modal'),
-        phone = document.querySelector('#phone');
-    // mobile.addEventListener('mouseover', () => {
-    //     modal.classList.toggle('modal_active');
-    // });
-    // modal.addEventListener('mousemove', () => {
-    //     modal.classList.toggle('modal_active');
-    // });
-    // // modal.addEventListener('mouseout', () => {
-    // //     modal.classList.remove('modal_active');
-    // // });
+        modalClose = document.querySelector('.modal__close');
+
+
+    mobile.addEventListener('mouseenter', () => {
+        modal.classList.toggle('modal_active');
+    });
+
+    modalClose.addEventListener('click', () => {
+        modal.classList.remove('modal_active');
+    });
+
+    document.addEventListener('scroll', () => {
+        if (window.pageYOffset > 100 && humburger.classList.contains('burger_active')) {
+            humburger.classList.remove('burger_active')
+            menu.classList.remove('header__item_active');
+            logoMobile.classList.remove('logo-mobile');
+        }
+    })
+
 
     showEmail.addEventListener('click', () => {
         copyBtn.classList.toggle('copy__btn_active');
