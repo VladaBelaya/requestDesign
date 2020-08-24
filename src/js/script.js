02e8ad1201b2535b28f909a3a9eadd81c5d7a022
Vlada <vlada_chvanova@mail.ru>
2020-08-24 13:55:30 +0300

    переделала блок рекомендаций

--- src/js/script.js
+++ src/js/script.js
@@ -14,10 +14,20 @@ let swiper = new Swiper('.swiper-container', {
 });
 
 
+
 window.addEventListener('DOMContentLoaded', () => {
     const menu = document.querySelector('.header__item'),
         menuItem = document.querySelectorAll('.header__item li'),
-        humburger = document.querySelector('.burger');
+        humburger = document.querySelector('.burger'),
+        showEmail = document.querySelector('.header__link'),
+        copyBtn = document.querySelector('.copy__btn');
+
+    showEmail.addEventListener('click', () => {
+        copyBtn.classList.toggle('copy__btn_active');
+    });
+    copyBtn.addEventListener('click', () => {
+        copyBtn.classList.remove('copy__btn_active');
+    });
 
     humburger.addEventListener('click', () => {
         humburger.classList.toggle('burger_active');
@@ -30,6 +40,7 @@ window.addEventListener('DOMContentLoaded', () => {
             humburger.classList.toggle('burger_active');
             menu.classList.toggle('header__item_active');
             document.querySelector('.logo').classList.toggle('logo-test');
+
         });
     });
 });
